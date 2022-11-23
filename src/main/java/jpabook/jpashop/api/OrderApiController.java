f669ab0dbecf85d206844b68a59439689f6f84c7
mingsound <62479120+mingsound21@users.noreply.github.com>
2022-11-23 17:03:00 +0900

    ch 04. 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,4 +35,52 @@ public class OrderApiController {
         return all;
     }
 
+    // SQL 수: 1(Order) + Order개수 x [N(Member:1, Order당 1명) + N(Delivery:1, Order당 1개) + N(OrderItems:1, 각 Order당 1개) + N(OrderItem:2, 각 OrderItems당 2개)]
+    // 즉, 2개의 주문이니까 1(order) + 2 x (1 + 1 + 1 + 2) = 1 + 10 = 11
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+    // BAD) 엔티티 외부로 노출 X
+    // Entity를 외부로 노출하지 말라는 것은 완전히 Entity에 대한 의존을 끊어야 한다는 말
+    // => OrderDto안에 OrderItem은 또 다시 Entity
+
+    @Data
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems; // List<OrderItem> ->  List<OrderItemDto> 변경
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+            order.getOrderItems().stream().forEach(o -> o.getItem().getName()); // OrderItems안에 있는 OrderItem도 엔티티라서 강제초기화 필요
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto{
+        private String itemName; //상품명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 }
